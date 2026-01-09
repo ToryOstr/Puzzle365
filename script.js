c5f8a292ca9a9cc8151da45094f7268417823ea3
ToryOstr <vik.ostrishko@gmail.com>
2026-01-09 15:25:41 +0200

    add rotation and mirroring logic

--- script.js
+++ script.js
@@ -60,17 +60,6 @@ function normalizeShape(shape) {
   let minY = Math.min(...shape.map(p => p[1]));
   return shape.map(([x, y]) => [x - minX, y - minY]);
 };
-function rotateShape90(shape) {
-  let maxX = Math.max(...shape.map(p => p[0]));
-  let rotated = shape.map(([x, y]) => [y, maxX - x]);
-  return normalizeShape(rotated);
-};
-
-function mirrorShape(shape) {
-  let maxX = Math.max(...shape.map(p => p[0]));
-  let mirrored = shape.map(([x, y]) => [maxX - x, y]);
-  return normalizeShape(mirrored);
-};
 
 // визначаємо розмір сітки для фігури
 
@@ -80,47 +69,81 @@ function gridSizeY(item) {
 function gridSizeX(item) {
   return Math.max(...item.map(([x, _]) => x)) + 1;
 };
-
-const itemsContainer = document.querySelector('.itemsContainer');
-
-//cтворюємо фігуру
 function createFigure(item, i) {
   let figure = '';
   item.forEach(([x, y]) => {
     figure += `<div class="cell" style="grid-area: ${x + 1} / ${y + 1}"></div>`;
   });
 
-  return `<div id='${i}' class="item" style="grid-template-rows: repeat(${gridSizeX(item)}, var(--fraction)); grid-template-columns: repeat(${gridSizeY(item)}, var(--fraction));">
+  return `<div id='${i}' class="item" draggable="true" style="grid-template-rows: repeat(${gridSizeX(item)}, var(--fraction)); grid-template-columns: repeat(${gridSizeY(item)}, var(--fraction));">
   ${figure}
 </div>`;
 
 };
+function rotateShape90(shape, id) {
+  let maxX = Math.max(...shape.map(p => p[0]));
+  let rotated = shape.map(([x, y]) => [y, maxX - x]);
+
+  return normalizeShape(rotated);
+};
+
+function mirrorShape(shape) {
+  let maxX = Math.max(...shape.map(p => p[0]));
+  let mirrored = shape.map(([x, y]) => [maxX - x, y]);
+  
+  return normalizeShape(mirrored);
+};
+
+
+const itemsContainer = document.querySelector('.itemsContainer');
+
+//cтворюємо фігуру
 // додаємо фігури на сторінку
 puzzleShapes.forEach((shape,i) => {
   itemsContainer.innerHTML += createFigure(shape, i);
 });
 
-
-//figure rotation
-
-
-
 let shapes = document.querySelectorAll('.item');
+let rotateBtn = document.querySelector('#rotateBtn');
+let mirrorBtn = document.querySelector('#mirrorBtn');
+
+let currentShape;
+let selectedShapeId = null;
+
+rotateBtn.addEventListener('click', () => {
+  if (selectedShapeId === null) return;
+
+  puzzleShapes[selectedShapeId] = rotateShape90(puzzleShapes[selectedShapeId]);
+  let inner = '';
+  puzzleShapes[selectedShapeId].forEach(([x, y]) => {
+    inner += `<div class="cell" style="grid-area: ${x + 1} / ${y + 1}"></div>`;
+  });
+  currentShape.innerHTML = inner;
+  currentShape.style.gridTemplateRows = `repeat(${gridSizeX(puzzleShapes[selectedShapeId])}, var(--fraction))`;
+  currentShape.style.gridTemplateColumns = `repeat(${gridSizeY(puzzleShapes[selectedShapeId])}, var(--fraction))`;
+
+});
+
+mirrorBtn.addEventListener('click', () => {
+  if (selectedShapeId === null) return;
+
+  puzzleShapes[selectedShapeId] = mirrorShape(puzzleShapes[selectedShapeId]);
+  let inner = '';
+  puzzleShapes[selectedShapeId].forEach(([x, y]) => {
+    inner += `<div class="cell" style="grid-area: ${x + 1} / ${y + 1}"></div>`;
+  });
+  currentShape.innerHTML = inner;
+  currentShape.style.gridTemplateRows = `repeat(${gridSizeX(puzzleShapes[selectedShapeId])}, var(--fraction))`;
+  currentShape.style.gridTemplateColumns = `repeat(${gridSizeY(puzzleShapes[selectedShapeId])}, var(--fraction))`;
+
+});
 
 shapes.forEach((shape) => {
   shape.addEventListener('click', (e) => {
-    const currentShape = e.currentTarget;
-    let id = currentShape.id;
-    puzzleShapes[id] = rotateShape90(puzzleShapes[id]);
-    let inner = '';
-    puzzleShapes[id].forEach(([x, y]) => {
-      inner += `<div class="cell" style="grid-area: ${x + 1} / ${y + 1}"></div>`;
-    });
-    currentShape.innerHTML = inner;
-    currentShape.style.gridTemplateRows = `repeat(${gridSizeX(puzzleShapes[id])}, var(--fraction))`;
-    currentShape.style.gridTemplateColumns = `repeat(${gridSizeY(puzzleShapes[id])}, var(--fraction))`;
+    currentShape = e.currentTarget;
+    selectedShapeId = currentShape.id;
 
-  })
+  });
 });
 
 // знаходимо комірку, по якій клікнули
